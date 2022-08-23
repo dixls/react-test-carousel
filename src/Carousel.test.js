196d19df07c593b746e17eb869ea0079facffc6e
pixls <tha.pixls@gmail.com>
2022-08-23 15:45:04 -0400

    added tests to check whether arrows visible when they should not be

--- src/Carousel.test.js
+++ src/Carousel.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { render, fireEvent, queryByTestId } from "@testing-library/react";
 import Carousel from "./Carousel";
 
 it("should load without crashing", () => {
@@ -45,4 +45,28 @@ it("works when you click on the left arrow", function() {
   // expect the first image to show, but not the second
   expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).toBeInTheDocument();
   expect(queryByAltText("Photo by Pratik Patel on Unsplash")).not.toBeInTheDocument();
+});
+
+it("should not show left arrow when first image is showing", () => {
+  
+  const leftArrow = queryByTestId("left-arrow");
+  expect(leftArrow).not.toBeInTheDocument();
+
+  // move forward in the carousel
+  const rightArrow = queryByTestId("right-arrow");
+  fireEvent.click(rightArrow);
+
+  expect(leftArrow).toBeInTheDocument();
+});
+
+it("should not show right arrow when last image is showing", () => {
+  
+  const rightArrow = queryByTestId("right-arrow");
+  expect(rightArrow).not.toBeInTheDocument();
+
+  // move forward to the end of the carousel
+  fireEvent.click(rightArrow);
+  fireEvent.click(rightArrow);
+
+  expect(rightArrow).not.toBeInTheDocument();
 });
